cb48969799b8adbde729ab2c226123a9568ec9f3
Hank <zakey.ken@foxmail.com>
2017-07-09 15:39:02 +0800

    完成nss_spider

--- Web_spider/pipelines.py
+++ Web_spider/pipelines.py
@@ -4,14 +4,30 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-from pymysql import connections
+from pymysql import connect
 
+from scrapy.log import logger
 from Web_spider.items import WebSpiderItem, CommitItem
 
 class WebSpiderPipeline(object):
 
     def __init__(self):
-        self.conn = connections
+        self.conn = connect(
+            host = 'rm-uf6vwd4k0y2o4b33bo.mysql.rds.aliyuncs.com',
+            port = 3306,
+            user = 'root',
+            password ='Myzh1987',
+            db = 'ca_immigration',
+            charset = 'utf8'
+        )
+        self.cursor = self.conn.cursor()
+        self.sql = "INSERT IGNORE INTO ns_data (`crawl_time`, `key_word`) VALUES (%s,%s)"
 
     def process_item(self, item, spider):
-        return item
+        try:
+            self.cursor.execute(self.sql, (item['crawl_time'], item['key_word']))
+            self.conn.commit()
+        except Exception , e:
+            logger.warning("execute sql fail.")
+            logger.warning(str(e))
+            logger.warning(item)
--- Web_spider/spiders/ns_spider.py
+++ Web_spider/spiders/ns_spider.py
@@ -7,11 +7,16 @@ from Web_spider.items import WebSpiderItem
 class NsSpiderSpider(scrapy.Spider):
     name = 'ns_spider'
     allowed_domains = ['http://novascotiaimmigration.com/move-here/nova-scotia-demand-express-entry/']
-    start_urls = ['http://http://novascotiaimmigration.com/move-here/nova-scotia-demand-express-entry//']
+    start_urls = ['http://novascotiaimmigration.com/move-here/nova-scotia-demand-express-entry/']
+    custom_settings = {
+        'ITEM_PIPELINES': {
+            'Web_spider.pipelines.WebSpiderPipeline': 300,
+        }
+    }
 
     def parse(self, response):
 
-        target_string = response.xpath("//article[@role='main']/div[@class='note']")[0].extract()
+        target_string = response.xpath("//article[@role='main']/div[@class='note']/p/strong/text()").extract()[0]
         item = WebSpiderItem()
         item['crawl_time'] = datetime.datetime.now()
         item['key_word'] = target_string
